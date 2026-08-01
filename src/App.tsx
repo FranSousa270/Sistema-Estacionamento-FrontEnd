470da6eaecb055cc2ee014147098c0430a03108a
Cris2700 <cristianoaquino270@gmail.com>
2026-08-01 15:39:11 -0300

    feat: estrutura inicial do frontend

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import { Card, CardHeader } from '@/components/ui/card'
+
+function App() {
+  
+
+  return (
+    <>
+    <Card>
+      <CardHeader>
+        <h2 className="text-lg font-semibold">Welcome to Vite</h2>
+      </CardHeader>
+    </Card>
+    </>
+  )
+}
+
+export default App
